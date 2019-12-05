19b59954ae022dac1d9e9b6c4099346e8be8f50f
Brandon <verv0022@algonquinlive.com>
2019-12-05 00:30:47 -0500

    Updates

---
+++ precache-manifest.26dec88d11d88f2a5e81acc820897e40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44d4b0255752f41e085fc6e7bc34b76",
+    "url": "/verv0022/mad9135-h5-choice/index.html"
+  },
+  {
+    "revision": "363705e28c67d35a64b9",
+    "url": "/verv0022/mad9135-h5-choice/static/css/main.e76308f6.chunk.css"
+  },
+  {
+    "revision": "8189db75349d7f455a8a",
+    "url": "/verv0022/mad9135-h5-choice/static/js/2.3f65c2a5.chunk.js"
+  },
+  {
+    "revision": "363705e28c67d35a64b9",
+    "url": "/verv0022/mad9135-h5-choice/static/js/main.c0aaaf8a.chunk.js"
+  },
+  {
+    "revision": "db8b722fddedddc1876d",
+    "url": "/verv0022/mad9135-h5-choice/static/js/runtime-main.4304006c.js"
+  }
+]);
