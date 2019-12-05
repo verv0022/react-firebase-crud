13f6f5f37721ba2673fbab862bbfcb5c949995ba
Brandon <verv0022@algonquinlive.com>
2019-12-05 00:38:34 -0500

    Updates

---
+++ precache-manifest.46c351b3e66a603620e9b64f8365d74f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952d6e8d0be1b0b9771265faa074f054",
+    "url": "/mad9135-h5-choice/index.html"
+  },
+  {
+    "revision": "5cb6798ceb8ccc1a75e4",
+    "url": "/mad9135-h5-choice/static/css/main.e76308f6.chunk.css"
+  },
+  {
+    "revision": "f6e3822ee89ef8a32f12",
+    "url": "/mad9135-h5-choice/static/js/2.81d53a67.chunk.js"
+  },
+  {
+    "revision": "5cb6798ceb8ccc1a75e4",
+    "url": "/mad9135-h5-choice/static/js/main.eedc4f6a.chunk.js"
+  },
+  {
+    "revision": "2b20eb2d9635a3cf7839",
+    "url": "/mad9135-h5-choice/static/js/runtime-main.68525d29.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/verv0022/mad9135-h5-choice/precache-manifest.26dec88d11d88f2a5e81acc820897e40.js"
+  "/mad9135-h5-choice/precache-manifest.46c351b3e66a603620e9b64f8365d74f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/verv0022/mad9135-h5-choice/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mad9135-h5-choice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
